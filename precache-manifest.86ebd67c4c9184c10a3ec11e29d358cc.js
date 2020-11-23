77b8cd7937c8d6148bf6d0bc2e9b08bf02ea715d
Fog <lin.emay@gmail.com>
2020-11-23 03:21:33 -0800

    Updates

---
+++ precache-manifest.86ebd67c4c9184c10a3ec11e29d358cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7565e3deaebab67465ec158d2af23480",
+    "url": "/user-directory/index.html"
+  },
+  {
+    "revision": "d0fbf3c17c4814e54f09",
+    "url": "/user-directory/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "b86b73332bd70e4744bb",
+    "url": "/user-directory/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "d0fbf3c17c4814e54f09",
+    "url": "/user-directory/static/js/2.b5411254.chunk.js"
+  },
+  {
+    "revision": "b86b73332bd70e4744bb",
+    "url": "/user-directory/static/js/main.723b17c2.chunk.js"
+  },
+  {
+    "revision": "42d996c3e5df8953289b",
+    "url": "/user-directory/static/js/runtime~main.eddb9125.js"
+  }
+]);
